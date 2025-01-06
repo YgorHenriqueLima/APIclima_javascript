0859c073125e16d49fb151e31ffe9bb9d19cc58b
Ygorhenrique <118612433+YgorHenriqueLima@users.noreply.github.com>
2025-01-06 13:59:23 -0300

    criando o conteúdo e o estilo da API

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// API KEY: 16e4406642778aa7d373213a89646453
+// funcionamento da chave: https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=16e4406642778aa7d373213a89646453 
+// -8.289255, -35.032022
+
